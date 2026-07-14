439dbe58719848dc317fc360a443a568ab6f4536
Rafael Alba <ralbas.int@gmail.com>
2026-07-14 10:11:13 +0200

    Export timeline API router

--- backend/src/trace_iam/api/__init__.py
+++ backend/src/trace_iam/api/__init__.py
@@ -4,10 +4,12 @@ from .evidence_routes import router as evidence_router
 from .guest_routes import router as guest_b2b_router
 from .resource_routes import router as resource_assignment_router
 from .routes import router as investigation_router
+from .timeline_routes import router as timeline_router
 
 __all__ = [
     "evidence_router",
     "guest_b2b_router",
     "investigation_router",
     "resource_assignment_router",
+    "timeline_router",
 ]
